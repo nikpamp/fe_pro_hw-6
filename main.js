cc4c0ff47256772da2046c40606f6177c3d4d2c2
nikita-pampushka <nikitapampushka@gmail.com>
2022-04-02 01:10:25 +0300

    Ready for Review

---
+++ main.js
@@ -0,0 +1,22 @@
+function numberCheck (number) {
+    number = +prompt('Enter your Number:');
+    function isEven (number) {
+        if(number % 2 === 0) {
+            return "even";
+        } else {
+            return "odd";
+        }
+    }
+
+    function isPrime (number) {
+        for (let i = 2; i < number; i++) {
+            if (number % i === 0) {
+                return "composite";
+            }
+        }
+        
+        return "prime";
+    }
+
+    alert(`Entered number is ${number}. This is ${isEven(number)} and ${isPrime(number)} number!`);
+}
